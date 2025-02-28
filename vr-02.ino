abeed94f86f618f7ac83212e11321f5324399899
Serg <sergw3x@gmail.com>
2025-02-28 16:14:41 +0300

    Update vr-02.ino

--- vr-02.ino
+++ vr-02.ino
@@ -1,14 +1,19 @@
-
+// pins DVR
 const uint8_t _pin_DVR = 4;      // PB4
-int state_pin_DVR = LOW;
-const uint8_t _pin_VRX = 3;      // PB3
-int state_pin_VRX = HIGH;
-const uint8_t _pin_BTN = 2;      // PB2
+const uint8_t _pin_DVR_LED = 1;  // PB1
 
-const uint8_t _pin_LED_DVR = 1;  // PB1
-int state_LED_DVR = HIGH;
-const uint8_t _pin_LED_VRX = 0;  // PB0
-int state_LED_VRX = LOW;
+// state DVR
+int state_pin_DVR = LOW;
+
+// pins VideoRX
+const uint8_t _pin_VRX = 3;      // PB3
+const uint8_t _pin_VRX_LED = 0;  // PB0
+
+// state VideoRX
+int state_pin_VRX = HIGH;
+
+// Button pin
+const uint8_t _pin_BTN = 2;      // PB2
 
 int buttonState;                 // the current reading from the input pin
 int lastButtonState = LOW;       // the previous reading from the input pin
@@ -17,15 +22,17 @@ unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 void setup() {
-  pinMode(_pin_LED_DVR, OUTPUT);
-  pinMode(_pin_LED_VRX, OUTPUT);
+  pinMode(_pin_DVR_LED, OUTPUT);
+  pinMode(_pin_VRX_LED, OUTPUT);
 
   pinMode(_pin_BTN, INPUT);
 
-  // enable led & open the channel
-  digitalWrite(_pin_LED_DVR, HIGH);
-  digitalWrite(_pin_LED_VRX, LOW);
-  digitalWrite(_pin_VRX, HIGH);
+  // enable led for default [VRX], enable LED & open the channel
+  digitalWrite(_pin_VRX_LED, state_pin_VRX);
+  digitalWrite(_pin_VRX, state_pin_VRX);
+
+  digitalWrite(_pin_DVR_LED, state_pin_DVR);  
+  digitalWrite(_pin_DVR, state_pin_DVR);
 }
 
 
@@ -46,35 +53,28 @@ void loop() {
       if (buttonState == HIGH) {
 
         if (state_pin_VRX == HIGH) {
+         
           // disable VideoRX
           state_pin_VRX = LOW;
-          state_LED_VRX = LOW;
-
           digitalWrite(_pin_VRX, state_pin_VRX);
-          digitalWrite(_pin_LED_VRX, state_LED_VRX);
+          digitalWrite(_pin_VRX_LED, state_pin_VRX);
           
           // enable DVR
           state_pin_DVR = HIGH;
-          state_LED_DVR = HIGH;
-          
           digitalWrite(_pin_DVR, state_pin_DVR);
-          digitalWrite(_pin_LED_DVR, state_LED_DVR);
+          digitalWrite(_pin_DVR_LED, state_pin_DVR);
 
         } else {
 
           // disable DVR
           state_pin_DVR = LOW;
-          state_LED_DVR = LOW;
-          
           digitalWrite(_pin_DVR, state_pin_DVR);
-          digitalWrite(_pin_LED_DVR, state_LED_DVR);
+          digitalWrite(_pin_DVR_LED, state_pin_DVR);
 
           // enable VideoRX
           state_pin_VRX = HIGH;
-          state_LED_VRX = HIGH;
-
           digitalWrite(_pin_VRX, state_pin_VRX);
-          digitalWrite(_pin_LED_VRX, state_LED_VRX);
+          digitalWrite(_pin_VRX_LED, state_pin_VRX);
           
         }
       }
