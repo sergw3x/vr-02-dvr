dbb6c485be083a095607330761928e249050cb7f
Shklyar Sergey <sergw3x@gmail.com>
2025-03-26 13:58:13 +0300

    new version

--- vr-02.ino
+++ vr-02.ino
@@ -1,84 +1,78 @@
 
-#define BOARD_VERSION 1
+#define _BTN_PIN PB0;
+#define _VRX_LED PB1;
+#define _DVR_LED PB2;
+#define _SW_PIN  PB4;
 
-#if BOARD_VERSION == 1
+#define _debounceDelay 100;
 
-#define VRX_PIN 3
-#define VRX_LED 1
-#define DVR_PIN 4
-#define DVR_LED 0
-#define BTN_PIN 2
+uint8_t debounceDelay = _debounceDelay;
 
-#elif BOARD_VERSION == 2
-
-#define VRX_PIN = 3;
-#define VRX_LED 1
-#define DVR_PIN 4
-#define DVR_LED 2
-#define BTN_PIN 0
-
-#endif
-
-const uint8_t buttonPin = BTN_PIN;  // the number of the pushbutton pin
-const uint8_t debounceDelay = 100;  // milliseconds the button must be in a stable state
-
-uint8_t buttonState = HIGH;     // the current reading from the input pin,  initial value = default (pullup, so high)
-uint8_t lastButtonState = LOW;  // the previous reading from the input pin, initial value = default (pullup, so high)
-
-uint32_t lastDebounceTime = 0;  // the last time the output pin was toggled
-uint8_t count = 0;
+uint8_t BTN_PIN = _BTN_PIN ;
+uint8_t VRX_LED = _VRX_LED;
+uint8_t DVR_LED = _DVR_LED ;
+uint8_t SW_PIN = _SW_PIN ;
 
 
-uint8_t vrx_status = HIGH;
-uint8_t dvr_status = LOW;
+uint32_t lastDebounceTime = 0;
+uint8_t buttonState = LOW;
+uint8_t lastButtonState = LOW;
 
-const uint8_t vrx_pin = VRX_PIN;
-const uint8_t vrx_led = VRX_LED;
-
-const uint8_t dvr_pin = DVR_PIN;
-const uint8_t dvr_led = DVR_LED;
-
+uint8_t sw_state = LOW;
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(dvr_led, OUTPUT);
-  pinMode(dvr_pin, OUTPUT);
-  pinMode(vrx_led, OUTPUT);
-  pinMode(vrx_pin, OUTPUT);
+  pinMode(BTN_PIN, INPUT);
+  pinMode(DVR_LED, OUTPUT);
+  pinMode(VRX_LED, OUTPUT);
+  pinMode(SW_PIN, OUTPUT);
 
-  // enable led for default [VRX], enable LED & open the channel
+  digitalWrite(VRX_LED, HIGH);
+  digitalWrite(DVR_LED, LOW);
+  digitalWrite(SW_PIN, sw_state);
+
+  blink();
 }
 
 void loop() {
 
-  if (digitalRead(buttonPin) != lastButtonState) {
-    lastButtonState = !lastButtonState;  // Switched states, HIGH becomes LOW, or LOW becomes HIGH
+  if (digitalRead(BTN_PIN) != lastButtonState) {
+    lastButtonState = !lastButtonState;
     lastDebounceTime = millis();
   } else if ((millis() - lastDebounceTime) > debounceDelay && buttonState != lastButtonState) {
     buttonState = lastButtonState;
-
-    // On the rising edge, that is when you release the button, toggle the LED
+    
     if (buttonState == HIGH) {
 
-      vrx_status = !vrx_status;
-      dvr_status = !dvr_status;
+      if (sw_state) {
+        sw_state = LOW;
+      } else {
+        sw_state = HIGH;
+      }
+
     }
   }
 
-  if (dvr_status == HIGH) {
-
-    digitalWrite(vrx_pin, vrx_status);
-    digitalWrite(vrx_led, vrx_status);
-
-    digitalWrite(dvr_pin, dvr_status);
-    digitalWrite(dvr_led, dvr_status);
-
+  if (sw_state) {
+    digitalWrite(VRX_LED, HIGH);
+    digitalWrite(DVR_LED, LOW);
+    sw_state = LOW;
   } else {
+    digitalWrite(VRX_LED, LOW);
+    digitalWrite(DVR_LED, HIGH);
+    sw_state = HIGH;
+  }
+  
+  digitalWrite(SW_PIN, sw_state);
 
-    digitalWrite(dvr_pin, dvr_status);
-    digitalWrite(dvr_led, dvr_status);
+}
 
-    digitalWrite(vrx_pin, vrx_status);
-    digitalWrite(vrx_led, vrx_status);
+void blink() {
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(VRX_LED, HIGH);
+    digitalWrite(DVR_LED, LOW);
+    delay(200);
+    digitalWrite(VRX_LED, LOW);
+    digitalWrite(DVR_LED, HIGH);
+    delay(200);
   }
 }
